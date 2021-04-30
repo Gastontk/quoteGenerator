c56ae39ae195a9b95660043815e68e992c94016f
Gaston Kennedy <gastonkennedy@Gastons-MacBook-Air.local>
2021-04-30 16:34:55 -0700

    Thought I should commit some changes

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log('testing')
+console.log('Quote Generator')
+
+const quoteContainer =document.getElementById('quote-container');
+const quoteText =document.getElementById('quote');
+const authorText =document.getElementById('author');
+const twitterBtn =document.getElementById('twitter');
+const newQuoteBtn =document.getElementById('new-quote');
+const loader = document.getElementById('loaderID');
+
+let apiQuotes = [];
+function loading(){
+    quoteContainer.classList.add('invisible');
+    loader.classList.remove('invisible');
+ }
+function complete(){
+    quoteContainer.classList.remove('invisible');
+    loader.classList.add('invisible');
+ }
+// Show new Quote
+function newQuote(){
+
+    const quote =  apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    console.log('And the quote is: ', quote)
+    quoteText.textContent=quote.text;
+    // check for null author
+    authorText.textContent=quote.author || 'Unknown Author';
+    //change class to shrink font when there is a long quote
+    quote.text.length > 150 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote') ;
+
+}
+
+//Get Quotes from API
+async function getQuotes() {
+    loading();
+    const ApiURL ='https://type.fit/api/quotes';
+    try {
+        quoteText.textContent="loading";
+        authorText.textContent="loading";
+
+        const response = await fetch(ApiURL)
+        apiQuotes = await response.json();
+        console.log(Math.floor(Math.random()*apiQuotes.length));
+        console.log ('quotes', apiQuotes[Math.floor(Math.random()*apiQuotes.length)])
+        newQuote( )
+        complete();
+
+    } catch (error){
+        // handle error here (alert or pass the error shere it could be useful)
+    }
+}
+// tweet a  quote
+
+function tweetQuote (){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    console.log(twitterUrl)
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listerns
+
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+//  On load
+getQuotes();
